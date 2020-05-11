8a843895e397929cae1d354938b83a1440e1103a
Anartz Mugika Ledo <mugan86@gmail.com>
2020-05-11 11:43:17 +0200

    Sección 11 - Clases 13-14

--- src/app/@admin/pages/users/users.component.ts
+++ src/app/@admin/pages/users/users.component.ts
@@ -1,3 +1,4 @@
+import { USERS_LIST_QUERY } from '@graphql/operations/query/user';
 import { Component, OnInit } from '@angular/core';
 import { DocumentNode } from 'graphql';
 import { IResultData } from '@core/interfaces/result-data.interface';
@@ -8,13 +9,20 @@ import { IResultData } from '@core/interfaces/result-data.interface';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-  query: DocumentNode;
+  query: DocumentNode = USERS_LIST_QUERY;
   context: object;
   itemsPage: number;
   resultData: IResultData;
   include: boolean;
 
   ngOnInit(): void {
+    this.context = {};
+    this.itemsPage = 10;
+    this.resultData = {
+      listKey: 'users',
+      definitionKey: 'users'
+    };
+    this.include = true;
   }
 
 }
--- src/app/@shared/table-pagination/table-pagination.component.ts
+++ src/app/@shared/table-pagination/table-pagination.component.ts
@@ -1,7 +1,9 @@
-import { USERS_LIST_QUERY } from './../../@graphql/operations/query/user';
 import { Component, OnInit, Input } from '@angular/core';
 import { DocumentNode } from 'graphql';
 import { TablePaginationService } from './table-pagination.service';
+import { IResultData, IInfoPage } from '@core/interfaces/result-data.interface';
+import { map } from 'rxjs/internal/operators/map';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-table-pagination',
@@ -9,23 +11,42 @@ import { TablePaginationService } from './table-pagination.service';
   styleUrls: ['./table-pagination.component.scss']
 })
 export class TablePaginationComponent implements OnInit {
-  @Input() query: DocumentNode = USERS_LIST_QUERY;
+  @Input() query: DocumentNode;
   @Input() context: object;
   @Input() itemsPage = 20;
   @Input() include = true;
+  @Input() resultData: IResultData;
+  infoPage: IInfoPage;
+  data$: Observable<any>;
   constructor(private service: TablePaginationService) { }
 
   ngOnInit(): void {
     if (this.query === undefined) {
       throw new Error('Query is undefined, please add');
     }
+    if (this.resultData === undefined) {
+      throw new Error('ResultData is undefined, please add');
+    }
+    this.infoPage = {
+      page: 1,
+      pages: 1,
+      itemsPage: this.itemsPage,
+      total: 1
+    };
     this.loadData();
   }
 
   loadData() {
-    this.service.getCollectionData(this.query, {include: this.include}, {}).subscribe(result => {
-      console.log(result);
-    });
+    const variables = {
+      page: this.infoPage.page,
+      itemsPage: this.infoPage.itemsPage,
+      include: this.include
+    };
+    this.data$ = this.service.getCollectionData(this.query, variables , {}).pipe(
+      map((result: any) => {
+        return result[this.resultData.definitionKey][this.resultData.listKey];
+      }
+    ));
   }
 
 }
