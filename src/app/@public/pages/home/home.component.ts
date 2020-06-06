9057d153432b40c3d5a11ae2c084914d1b97b46a
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-06 11:03:41 +0200

    Sección 16 - Clases 2,3,4 - Instalaciones, configuraciones y añadir los elementos iniciales del carousel y los productos con los datos fake

--- src/app/@public/pages/home/home.component.ts
+++ src/app/@public/pages/home/home.component.ts
@@ -1,22 +1,34 @@
+import { ICarouselItem } from '@mugan86/ng-shop-ui/lib/interfaces/carousel-item.interface';
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '@graphql/services/api.service';
 import { AuthService } from '@core/services/auth.service';
 import { UsersService } from '@core/services/users.service';
-
+import carouselItems from '@data/carousel.json';
+import productsList from '@data/products.json';
+import { IProduct } from '@mugan86/ng-shop-ui/lib/interfaces/product.interface';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  items: ICarouselItem[] = [];
+  productsList;
   constructor(private usersApi: UsersService, private auth: AuthService) { }
 
   ngOnInit(): void {
-
+    this.productsList = productsList;
+    this.items = carouselItems;
+    console.log('Carousel items', this.items);
+    console.log('Products items', this.productsList);
     this.usersApi.getUsers(2, 1).subscribe( result => {
       // console.log(result); // { { status message users: []}
     });
+  }
+  addToCart($event: IProduct) {
+      console.log('Add to cart', $event);
+  }
+  showProductDetails($event: IProduct) {
+    console.log('Shoe details', $event);
   }
 
 }
--- src/app/@public/pages/home/home.module.ts
+++ src/app/@public/pages/home/home.module.ts
@@ -1,15 +1,17 @@
+import { CarouselItemsModule, ProductItemModule } from '@mugan86/ng-shop-ui';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
-
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    CarouselItemsModule,
+    ProductItemModule
   ]
 })
 export class HomeModule { }
