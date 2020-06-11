e2db620f095dd7ec594f95299489ad9b78072884
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-11 10:17:05 +0200

    Sección 17 - Clase 4 - Práctica - Carga de items del menu (ADMIN) de manera dinamica desde el JSON de configuración

--- src/app/@admin/core/components/sidebar/sidebar.component.ts
+++ src/app/@admin/core/components/sidebar/sidebar.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IMenuItem } from '@core/interfaces/menu-item.interface';
+import adminMenuItems from '@data/menus/admin.json';
 @Component({
   selector: 'app-admin-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SidebarComponent {
+  menuItems: Array<IMenuItem> = adminMenuItems;
 }
