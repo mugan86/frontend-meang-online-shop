c09d014aec72b0457bad7079100e710ed77c9d05
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-11 10:28:29 +0200

    Sección 17 - Clase 5 - Cargando información de productos en la página (estáticos los valores por ahora)

--- src/app/@public/pages/games/games.component.ts
+++ src/app/@public/pages/games/games.component.ts
@@ -1,4 +1,7 @@
+import { ACTIVE_FILTERS } from './../../../@core/constants/filters';
+import { ProductsService } from '@core/services/products.service';
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '@mugan86/ng-shop-ui/lib/interfaces/product.interface';
 
 @Component({
   selector: 'app-games',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./games.component.scss']
 })
 export class GamesComponent implements OnInit {
-
-  constructor() { }
+  productsList: Array<IProduct> = [];
+  constructor(private products: ProductsService) { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
+    this.products.getByPlatform(
+      1, 20, ACTIVE_FILTERS.ACTIVE,
+      false, '18'
+    ).subscribe(result => {
+      console.log('products ps4', result);
+      this.productsList = result;
+    });
   }
 
 }
--- src/app/@public/pages/games/games.module.ts
+++ src/app/@public/pages/games/games.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { GamesRoutingModule } from './games-routing.module';
 import { GamesComponent } from './games.component';
-
+import { ProductCategoryListModule } from '@core/components/product-category-list/product-category-list.module';
 
 @NgModule({
   declarations: [GamesComponent],
   imports: [
     CommonModule,
-    GamesRoutingModule
+    GamesRoutingModule,
+    ProductCategoryListModule
   ]
 })
 export class GamesModule { }
