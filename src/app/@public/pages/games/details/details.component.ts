d27a4421ce48530d519068903261d1a0a8ba7ef6
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-14 10:52:20 +0200

    Sección 18 - Clase 4 - Añadir información del producto desde el fichero products.json para ir estruturando

--- src/app/@public/pages/games/details/details.component.ts
+++ src/app/@public/pages/games/details/details.component.ts
@@ -1,12 +1,15 @@
+import { CURRENCIES_SYMBOL, CURRENCY_LIST } from '@mugan86/ng-shop-ui';
 import { Component, OnInit } from '@angular/core';
-
+import products from '@data/products.json';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
-
+  product = products[Math.floor(Math.random() * products.length)];
+  selectImage = this.product.img;
+  currencySelect = CURRENCIES_SYMBOL[CURRENCY_LIST.EURO];
   constructor() { }
 
   ngOnInit(): void {
