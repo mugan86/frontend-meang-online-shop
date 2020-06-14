1ef87ca6420e7fcbb8b73c42c30a427e5a30a98e
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-14 11:42:52 +0200

    Sección 18 - Clase 7.- Cargando los screens en la foto principal y ultimando detalles

--- src/app/@public/pages/games/details/details.component.ts
+++ src/app/@public/pages/games/details/details.component.ts
@@ -10,10 +10,23 @@ export class DetailsComponent {
   product = products[Math.floor(Math.random() * products.length)];
   selectImage = this.product.img;
   currencySelect = CURRENCIES_SYMBOL[CURRENCY_LIST.EURO];
+  screens = [
+    'https://media.rawg.io/media/games/e49/e49256363789d1dec316edf8ebb27ccf.jpg',
+    'https://media.rawg.io/media/screenshots/ead/ead0a4f9dcdd1084fbb0da8ea5a26a5e.jpg',
+    'https://media.rawg.io/media/screenshots/ff9/ff9005e66eefc3e9e908f8a918a575aa.jpg',
+    'https://media.rawg.io/media/screenshots/05b/05b1ddd7e6a33f716be0d6c6fa8d1b8c.jpg',
+    'https://media.rawg.io/media/screenshots/874/8749c4db3181be3ac374a0fd3d63a4f7.jpg',
+    'https://media.rawg.io/media/screenshots/6ce/6ce002980f0e02d3b9e57308ee85c220.jpg',
+    'https://media.rawg.io/media/screenshots/31e/31e4803dc0504d17b9b90fb0494a7a56.jpg'
+  ];
   constructor() { }
 
   changeValue(qty: number) {
     console.log(qty);
+  }
+
+  selectImgMain(i) {
+    this.selectImage = this.screens[i];
   }
 
 }
