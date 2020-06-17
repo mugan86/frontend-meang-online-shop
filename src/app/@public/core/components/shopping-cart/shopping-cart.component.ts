71a7cc6d5702e43a42eb75bdc575032b44dc0b09
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-17 10:13:31 +0200

    Sección 19 - Clase 3 -Creando menú lateral

--- src/app/@public/core/components/shopping-cart/shopping-cart.component.ts
+++ src/app/@public/core/components/shopping-cart/shopping-cart.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.scss']
+  styleUrls: ['./shopping-cart.component.scss'],
 })
 export class ShoppingCartComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openNav() {
+    console.log('Open nav');
+    document.getElementById('mySidenav').style.width = '250px';
   }
 
+  closeNav() {
+    console.log('Close nav');
+  }
 }
