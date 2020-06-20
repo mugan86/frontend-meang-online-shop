2070ac36ddd05493e0870b198ffe770050e2bf38
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-20 18:24:03 +0200

    Sección 20 Clase 6 - Obtener datos de autenticación y en el caso de no tener sesión activa, no dejar acceder al formulario de pago

--- src/app/@public/pages/forms/checkout/checkout.component.ts
+++ src/app/@public/pages/forms/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
+import { IMeData } from '@core/interfaces/session.interface';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
+  meData: IMeData;
+  constructor(private auth: AuthService, private router: Router) {
+    this.auth.accessVar$.subscribe((data: IMeData) => {
+      if (!data.status) {
+        // Ir a login
+        this.router.navigate(['/login']);
+        return;
+      }
+      this.meData = data;
+    });
+  }
 
   ngOnInit(): void {
+    this.auth.start();
   }
 
 }
