9c0642d2ef711f8eaaddb2339e3e9f621f322b56
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-20 19:56:41 +0200

    Sección 20 Clase 10 - Terminada sección - Últimando detalles con la sincronización del carrito de compra

--- src/app/@public/pages/games/details/details.component.ts
+++ src/app/@public/pages/games/details/details.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 import { loadData, closeAlert } from '@shared/alerts/alerts';
 import { CURRENCY_SELECT } from '@core/constants/config';
 import { CartService } from '@shop/core/services/cart.service.ts.service';
+import { ICart } from '@shop/core/components/shopping-cart/shoppin-cart.interface';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -30,12 +31,28 @@ export class DetailsComponent implements OnInit {
       this.loading = true;
       this.loadDataValue(+params.id);
     });
+
+    this.cartService.itemsVar$.subscribe((data: ICart) => {
+      console.log(data);
+      if (data.subtotal === 0) {
+        this.product.qty = 1;
+        return;
+      }
+
+      this.product.qty = this.findProduct(+this.product.id).qty;
+    });
+  }
+
+  findProduct(id: number) {
+    return this.cartService.cart.products.find( item => +item.id === id);
   }
 
   loadDataValue(id: number) {
     this.productService.getItem(id).subscribe((result) => {
       console.log(result);
       this.product = result.product;
+      const saveProductInCart = this.findProduct(+this.product.id);
+      this.product.qty = (saveProductInCart !== undefined) ? saveProductInCart.qty : this.product.qty;
       this.selectImage = this.product.img;
       this.screens = result.screens;
       this.relationalProducts = result.relational;
