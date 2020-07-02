e1f2f4cd912ffb9373319d4305be386438b7e210
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-02 11:22:53 +0200

    Sección 22 - Manejar la información y cuando tenemos todo, tenerlo para enviarlo

--- src/app/@public/pages/forms/checkout/checkout.component.ts
+++ src/app/@public/pages/forms/checkout/checkout.component.ts
@@ -13,6 +13,8 @@ import { StripePaymentService } from '@mugan86/stripe-payment-form';
 export class CheckoutComponent implements OnInit {
   meData: IMeData;
   key = environment.stripeKey;
+  address = '';
+  token = '';
   constructor(private auth: AuthService, private router: Router,
               private stripePaymentService: StripePaymentService) {
     this.auth.accessVar$.subscribe((data: IMeData) => {
@@ -22,6 +24,13 @@ export class CheckoutComponent implements OnInit {
         return;
       }
       this.meData = data;
+    });
+
+    this.stripePaymentService.cardTokenVar$.subscribe((token: string) => {
+      this.token = token;
+      if (this.token.indexOf('tok_') > -1 && this.meData.status && this.address !== '') {
+        console.log('Preparado para enviar la info del pedido');
+      }
     });
   }
 
--- src/app/@public/pages/forms/checkout/checkout.module.ts
+++ src/app/@public/pages/forms/checkout/checkout.module.ts
@@ -5,12 +5,14 @@ import { CheckoutRoutingModule } from './checkout-routing.module';
 import { CheckoutComponent } from './checkout.component';
 import { CheckoutResumeModule } from './checkout-resume/checkout-resume.module';
 import { StripePaymentFormModule } from '@mugan86/stripe-payment-form';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [CheckoutComponent],
   imports: [
     CommonModule,
     CheckoutRoutingModule,
     CheckoutResumeModule,
+    FormsModule,
     StripePaymentFormModule
   ]
 })
