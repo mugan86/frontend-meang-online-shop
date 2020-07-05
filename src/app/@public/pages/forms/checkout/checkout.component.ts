47df11736a050419488c3e43b5ca51e157ff86b4
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-05 09:48:47 +0200

    Sección 22 - Clase 2 - Comenzando con la integración de Stripe en Angular y primeros pasos hasta generar el token de la tarjeta

--- src/app/@public/pages/forms/checkout/checkout.component.ts
+++ src/app/@public/pages/forms/checkout/checkout.component.ts
@@ -2,6 +2,8 @@ import { IMeData } from '@core/interfaces/session.interface';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { StripePaymentService } from '@mugan86/stripe-payment-form';
 
 @Component({
   selector: 'app-checkout',
@@ -10,7 +12,9 @@ import { Router } from '@angular/router';
 })
 export class CheckoutComponent implements OnInit {
   meData: IMeData;
-  constructor(private auth: AuthService, private router: Router) {
+  key = environment.stripePublicKey;
+  constructor(private auth: AuthService, private router: Router,
+              private stripePayment: StripePaymentService) {
     this.auth.accessVar$.subscribe((data: IMeData) => {
       if (!data.status) {
         // Ir a login
@@ -19,11 +23,19 @@ export class CheckoutComponent implements OnInit {
       }
       this.meData = data;
     });
+
+    this.stripePayment.cardTokenVar$.subscribe((token: string) => {
+      console.log(token);
+    });
   }
 
   ngOnInit(): void {
     this.auth.start();
     localStorage.removeItem('route_after_login');
+  }
+
+  sendData() {
+    this.stripePayment.takeCardToken(true);
   }
 
 }
--- src/app/@public/pages/forms/checkout/checkout.module.ts
+++ src/app/@public/pages/forms/checkout/checkout.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CheckoutRoutingModule } from './checkout-routing.module';
 import { CheckoutComponent } from './checkout.component';
 import { CheckoutResumeModule } from './checkout-resume/checkout-resume.module';
-
+import { StripePaymentFormModule } from '@mugan86/stripe-payment-form';
 @NgModule({
   declarations: [CheckoutComponent],
   imports: [
     CommonModule,
     CheckoutRoutingModule,
-    CheckoutResumeModule
+    CheckoutResumeModule,
+    StripePaymentFormModule
   ]
 })
 export class CheckoutModule { }
