bc1f97d910e9eeb0c6b41189d00a20f81e6263f1
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-07 10:11:33 +0200

    Sección 22 - Clases 10-11 - Mejorar feedback con alerts y gestionar para que no podamos estar en la página del formulario de pago si no hay items en el carrito

--- src/app/@public/pages/forms/checkout/checkout.component.ts
+++ src/app/@public/pages/forms/checkout/checkout.component.ts
@@ -12,21 +12,26 @@ import { CustomerService } from '@shop/core/services/stripe/customer.service';
 import { TYPE_ALERT } from '@shared/alerts/values.config';
 import { ChargeService } from '@shop/core/services/stripe/charge.service';
 import { IPayment } from '@core/interfaces/stripe/payment.interface';
+import { ICart } from '@shop/core/components/shopping-cart/shoppin-cart.interface';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
   meData: IMeData;
   key = environment.stripePublicKey;
   address = '';
-  constructor(private auth: AuthService, private router: Router,
-              private stripePayment: StripePaymentService,
-              private cartService: CartService,
-              private customerService: CustomerService,
-              private chargeService: ChargeService) {
+  available = false;
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private stripePayment: StripePaymentService,
+    private cartService: CartService,
+    private customerService: CustomerService,
+    private chargeService: ChargeService
+  ) {
     this.auth.accessVar$.subscribe((data: IMeData) => {
       if (!data.status) {
         // Ir a login
@@ -36,66 +41,128 @@ export class CheckoutComponent implements OnInit {
       this.meData = data;
     });
 
-    this.stripePayment.cardTokenVar$.pipe(take(1)).subscribe((token: string) => {
-      if (token.indexOf('tok_') > -1 && this.meData.status && this.address !== '') {
-        // Almacenar la información para enviar
-        const payment: IPayment = {
-          token,
-          amount: this.cartService.cart.total.toString(),
-          description: this.cartService.orderDescription(),
-          customer: this.meData.user.stripeCustomer,
-          currency: CURRENCY_CODE
-        };
-        // Enviar la información y procesar el pago
-        this.chargeService.pay(payment).pipe(take(1))
-        .subscribe((result: {
-          status: boolean,
-          message: string,
-          charge: object
-        }) => {
-          if (result.status) {
-            console.log('OK');
-            console.log(result.charge);
-          } else {
-            console.log(result.message);
-          }
-        });
+    this.cartService.itemsVar$.pipe(take(1)).subscribe((cart: ICart) => {
+      if (this.cartService.cart.total === 0) {
+        this.available = false;
+        this.notAvailableProducts();
       }
     });
+
+    this.stripePayment.cardTokenVar$
+      .pipe(take(1))
+      .subscribe((token: string) => {
+        if (
+          token.indexOf('tok_') > -1 &&
+          this.meData.status &&
+          this.address !== ''
+        ) {
+          if (this.cartService.cart.total === 0) {
+            this.available = false;
+            this.notAvailableProducts();
+          }
+          // Almacenar la información para enviar
+          const payment: IPayment = {
+            token,
+            amount: this.cartService.cart.total.toString(),
+            description: this.cartService.orderDescription(),
+            customer: this.meData.user.stripeCustomer,
+            currency: CURRENCY_CODE,
+          };
+          loadData(
+            'Realizando el pago',
+            'Espera mienytras se procesa la información de pago'
+          );
+          // Enviar la información y procesar el pago
+          this.chargeService
+            .pay(payment)
+            .pipe(take(1))
+            .subscribe(
+              async (result: {
+                status: boolean;
+                message: string;
+                charge: object;
+              }) => {
+                if (result.status) {
+                  console.log('OK');
+                  console.log(result.charge);
+                  await infoEventAlert(
+                    'Pedido realizado correctamente',
+                    'Has efectuado correctamente el pedido. ¡¡Muchas gracias!!',
+                    TYPE_ALERT.SUCCESS
+                  );
+                  this.cartService.clear();
+                } else {
+                  console.log(result.message);
+                  await infoEventAlert(
+                    'Pedido NO SE HA realizado',
+                    'El pedido no se ha completado. Inténtelo de nuevo por favor',
+                    TYPE_ALERT.SUCCESS
+                  );
+                }
+              }
+            );
+        }
+      });
+  }
+
+ async notAvailableProducts() {
+    this.cartService.close();
+    this.available = false;
+    await infoEventAlert(
+      'Acción no disponible',
+      'No puedes realizar el pago sin productos en el carrito de la compra'
+    );
+    this.router.navigate(['/']);
   }
 
   ngOnInit(): void {
     this.auth.start();
-    if ( localStorage.getItem('address')) {
+    if (localStorage.getItem('address')) {
       this.address = localStorage.getItem('address');
       localStorage.removeItem('address');
     }
     this.cartService.initialize();
     localStorage.removeItem('route_after_login');
+    if (this.cartService.cart.total === 0) {
+      this.available = false;
+      this.notAvailableProducts();
+    } else {
+      this.available = true;
+    }
   }
 
   async sendData() {
     if (this.meData.user.stripeCustomer === null) {
       // Alerta para mostrar info
-      await infoEventAlert('Cliente no existe', 'Necesitamos un cliente para realizar el pago');
+      await infoEventAlert(
+        'Cliente no existe',
+        'Necesitamos un cliente para realizar el pago'
+      );
       const stripeName = `${this.meData.user.name} ${this.meData.user.lastname}`;
       loadData('Procesando la información', 'Creando el cliente...');
-      this.customerService.add(
-        stripeName,
-        this.meData.user.email
-      ).pipe(take(1)).subscribe(async (result: { status: boolean, message: string}) => {
-        if (result.status) {
-          await infoEventAlert('Cliente añadido al usuario', 'Reiniciar la sesión', TYPE_ALERT.SUCCESS);
-          localStorage.setItem('address', this.address);
-          localStorage.setItem('route_after_login', this.router.url);
-          this.auth.resetSession();
-        } else {
-          await infoEventAlert('Cliente no añadido', result.message, TYPE_ALERT.WARNING);
-        }
-      });
+      this.customerService
+        .add(stripeName, this.meData.user.email)
+        .pipe(take(1))
+        .subscribe(async (result: { status: boolean; message: string }) => {
+          if (result.status) {
+            await infoEventAlert(
+              'Cliente añadido al usuario',
+              'Reiniciar la sesión',
+              TYPE_ALERT.SUCCESS
+            );
+            localStorage.setItem('address', this.address);
+            localStorage.setItem('route_after_login', this.router.url);
+            this.auth.resetSession();
+          } else {
+            await infoEventAlert(
+              'Cliente no añadido',
+              result.message,
+              TYPE_ALERT.WARNING
+            );
+          }
+        });
       return;
     }
     this.stripePayment.takeCardToken(true);
   }
-
 }
