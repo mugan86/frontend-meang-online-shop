1a23b6f6165c3368d320d663b12fcd776f94accc
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-08 12:02:34 +0200

    Sección 22 Clase 21 - Inicializar sesión y con esa información obtener los datos relacionados al cliente

--- src/app/@public/pages/orders/orders.component.ts
+++ src/app/@public/pages/orders/orders.component.ts
@@ -1,15 +1,47 @@
+import { take } from 'rxjs/internal/operators/take';
+import { IMeData } from '@core/interfaces/session.interface';
 import { Component, OnInit } from '@angular/core';
+import { CURRENCY_SELECT } from '@core/constants/config';
+import { ICharge } from '@core/interfaces/stripe/charge.interface';
+import { AuthService } from '@core/services/auth.service';
+import { ChargeService } from '@shop/core/services/stripe/charge.service';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.scss']
+  styleUrls: ['./orders.component.scss'],
 })
 export class OrdersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  currencySymbol = CURRENCY_SELECT;
+  meData: IMeData;
+  startingAfter = '';
+  hasMore = false;
+  charges: Array<ICharge> = [];
+  constructor(
+    private auth: AuthService,
+    private chargeService: ChargeService
+  ) {
+    this.auth.accessVar$.pipe(take(1)).subscribe((meData: IMeData) => {
+      this.meData = meData;
+      // Si tenemos información cargar con el cliente
+      if (this.meData.user.stripeCustomer !== '') {
+        console.log(this.meData);
+        this.loadData();
+      }
+    });
   }
 
+  ngOnInit(): void {
+    this.auth.start();
+  }
+
+  loadData() {
+    console.log('Cargando datos...');
+    this.chargeService.listByCustomer(
+      this.meData.user.stripeCustomer,
+      10, this.startingAfter, ''
+    ).pipe(take(1)).subscribe((data: {hasMore: boolean, charges: Array<ICharge>}) => {
+      console.log(data);
+    });
+  }
 }
