355f7949a9b3a643ac9855cc1503c93f6860a25d
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-08 13:56:56 +0200

    Sección 22 - Clase 23 - Aplicar mejora de carga de datos en diferentes páginas

--- src/app/@public/pages/orders/orders.component.ts
+++ src/app/@public/pages/orders/orders.component.ts
@@ -5,6 +5,7 @@ import { CURRENCY_SELECT } from '@core/constants/config';
 import { ICharge } from '@core/interfaces/stripe/charge.interface';
 import { AuthService } from '@core/services/auth.service';
 import { ChargeService } from '@shop/core/services/stripe/charge.service';
+import { loadData, closeAlert } from '@shared/alerts/alerts';
 
 @Component({
   selector: 'app-orders',
@@ -17,6 +18,8 @@ export class OrdersComponent implements OnInit {
   startingAfter = '';
   hasMore = false;
   charges: Array<ICharge> = [];
+  loading = true;
+  loadMoreBtn = false;
   constructor(
     private auth: AuthService,
     private chargeService: ChargeService
@@ -26,7 +29,7 @@ export class OrdersComponent implements OnInit {
       // Si tenemos información cargar con el cliente
       if (this.meData.user.stripeCustomer !== '') {
         console.log(this.meData);
-        this.loadData();
+        this.loadChargesData();
       }
     });
   }
@@ -35,14 +38,25 @@ export class OrdersComponent implements OnInit {
     this.auth.start();
   }
 
-  loadData() {
-    console.log('Cargando datos...');
+  loadChargesData() {
+    loadData('Cargando...', 'Espera mientras carga los pedidos...');
     this.chargeService.listByCustomer(
       this.meData.user.stripeCustomer,
       10, this.startingAfter, ''
     ).pipe(take(1)).subscribe((data: {hasMore: boolean, charges: Array<ICharge>}) => {
-      console.log(data);
-      this.charges = data.charges;
+      /*console.log(data);
+      this.charges = data.charges;*/
+      data.charges.map((item: ICharge) => this.charges.push(item));
+      this.hasMore = data.hasMore;
+      if (this.hasMore) {
+        this.startingAfter = data.charges[data.charges.length - 1].id;
+        this.loadMoreBtn = true;
+      } else {
+        this.loadMoreBtn = false;
+        this.startingAfter = '';
+      }
+      closeAlert();
+      this.loading = false;
     });
   }
 }
