5498d2dd1563e0a008840f711a732da6e7e397b4
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-14 13:30:48 +0200

    Sección 25 - Clase 6 - Optimizando aplicación - Menu usuario en el header

--- src/app/@admin/core/components/header/header.component.ts
+++ src/app/@admin/core/components/header/header.component.ts
@@ -1,12 +1,28 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { AuthService } from '@core/services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-admin-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   toggledValue = true;
+  userLabel: string;
   @Output() toggleChange = new EventEmitter<boolean>();
+  constructor(private authService: AuthService, private router: Router) {
+    this.authService.accessVar$.subscribe((result) => {
+      console.log('dddd', result);
+      if (!result.status) {
+        this.router.navigate(['/']);
+        return;
+      }
+      this.userLabel = `${ result.user?.name } ${ result.user?.lastname }`;
+    });
+  }
+  ngOnInit() {
+    this.authService.start();
+  }
 
   toggled() {
     if (this.toggledValue === undefined) {
@@ -14,5 +30,9 @@ export class HeaderComponent {
     }
     this.toggledValue = !this.toggledValue;
     this.toggleChange.emit(this.toggledValue);
+  }
+
+  logout() {
+    this.authService.resetSession();
   }
 }
