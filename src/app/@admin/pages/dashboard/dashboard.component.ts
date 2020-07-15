ca290fc65ccd6207770416b3135971bd1401be48
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-15 11:13:42 +0200

    Sección 25 - Clase 12 - Pintar los datos de las estadísiticas en el dashboard - Final Sección

--- src/app/@admin/pages/dashboard/dashboard.component.ts
+++ src/app/@admin/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { TitleService } from '@admin/core/services/title.service';
 import { LABEL } from '@admin/core/constants/title.constants';
 import { IGeneralInfo } from '@shared/general-info/general-info.interface';
 import { AdminService } from '@admin/core/services/admin.service';
+import { loadData, closeAlert } from '@shared/alerts/alerts';
 
 @Component({
   selector: 'app-dashboard',
@@ -42,12 +43,17 @@ export class DashboardComponent implements OnInit {
       value: 'platforms'
     }
   ];
+  loading = true;
   constructor(private titleService: TitleService, private adminService: AdminService) { }
 
   ngOnInit(): void {
+    loadData('Cargando datos', 'Espera mientras se cargan las estadísticas');
     this.titleService.updateTitle(LABEL.DASHBOARD);
+    this.loading = true;
     this.adminService.getStats().subscribe((data) => {
-      console.log(data);
+      this.loading = false;
+      this.items.map((item) => item.value = data[item.value]);
+      closeAlert();
     });
   }
 
