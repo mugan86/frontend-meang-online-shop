04932d3e3bb441a7f9a881185098098c4e53ae2d
Anartz Mugika Ledo <mugan86@gmail.com>
2020-08-28 07:31:04 +0200

    Update GraphQL Connection configuration

--- src/app/@graphql/modules/graphql.module.ts
+++ src/app/@graphql/modules/graphql.module.ts
@@ -2,47 +2,52 @@ import { getMainDefinition } from 'apollo-utilities';
 import { WebSocketLink } from 'apollo-link-ws';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { onError } from 'apollo-link-error';
-import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
-import { Apollo, ApolloModule } from 'apollo-angular';
 import { NgModule } from '@angular/core';
 import { ApolloLink, split } from 'apollo-link';
 import { HttpClientModule } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 @NgModule({
   imports: [HttpClientModule, ApolloModule, HttpLinkModule],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory: (httpLink: HttpLink) => {
+      return {
+        cache: new InMemoryCache(),
+        link: getLink(httpLink)
+      };
+    },
+    deps: [HttpLink]
+  }],
 })
-export class GraphqlModule {
-  constructor(apollo: Apollo, httpLink: HttpLink) {
-    // Para capturar los errores de consulta y/o de red
-    const errorLink = onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors) {
-        console.log('GraphQL Errors', graphQLErrors);
-      }
+export class GraphqlModule { }
 
-      if (networkError) {
-        console.log('Network Errors', networkError);
-      }
-    });
-    const uri = environment.backend;
-    const urlLink = ApolloLink.from([errorLink, httpLink.create({ uri })]);
-    const subscriptionLink = new WebSocketLink({
-      uri: environment.backendWs,
-      options: {
-        reconnect: true
-      }
-    });
-    const link = split(
-      ({query}) => {
-        const { kind, operation}: any = getMainDefinition(query);
-        return kind === 'OperationDefinition' &&  operation === 'subscription';
-      },
-      subscriptionLink,
-      urlLink
-    );
-    apollo.create({
-      link,
-      cache: new InMemoryCache(),
-    });
-  }
+function getLink(httpLink: HttpLink) {
+  // Para capturar los errores de consulta y/o de red
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      console.log('GraphQL Errors', graphQLErrors);
+    }
+
+    if (networkError) {
+      console.log('Network Errors', networkError);
+    }
+  });
+  const uri = environment.backend;
+  const urlLink = ApolloLink.from([errorLink, httpLink.create({ uri })]);
+  const subscriptionLink = new WebSocketLink({
+    uri: environment.backendWs,
+    options: {
+      reconnect: true
+    }
+  });
+  return split(
+    ({query}) => {
+      const { kind, operation}: any = getMainDefinition(query);
+      return kind === 'OperationDefinition' &&  operation === 'subscription';
+    },
+    subscriptionLink,
+    urlLink
+  );
 }
