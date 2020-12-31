9e56d94a9774656597f46b3ea620fd0cad1ca47d
Anartz Mugika Ledo <mugan86@gmail.com>
2020-12-31 20:17:34 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backend: 'https://gamezonia.herokuapp.com/graphql',
-  backendWs: 'wss://gamezonia.herokuapp.com/graphql',
+  backend: 'https://gamezonia-api.herokuapp.com/graphql',
+  backendWs: 'wss://gamezonia-api.herokuapp.com/graphql',
   stripePublicKey: 'pk_test_51GzcRcEl8FzvNIw5T0Gq7wKpsnq6vhyGYYsyGJI3I2OpGWeroKg5k9frcVQHvHXAsn2PbzxIwOcKp5uFrhgmy9gd007i2WGOgP'
 };
